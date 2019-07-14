9fce882cf253ccb43121222ceeeec91cf7b3bdad
oberdev <qovalski@gmail.com>
2019-07-14 17:19:36 +0300

    make local registration route works

--- views/welcome.py
+++ views/welcome.py
@@ -1,5 +1,18 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot, Update
+
+button_list = [
+    [
+        InlineKeyboardButton("authenticate", callback_data="authenticate")
+    ]
+]
+
+markup = InlineKeyboardMarkup(button_list)
 
 
-def welcome(bot, update):
+def welcome(bot: Bot, update: Update):
     bot.send_message(
-        chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+        reply_to_message_id=update.message.message_id,
+        chat_id=update.message.chat_id,
+        text="I'm a bot, please talk to me!",
+        reply_markup=markup
+    )
